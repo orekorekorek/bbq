6f15d3187c720ac32a31779166e4e4b59b3d29d7
Kirill Kuznetsov <shapowgorillalord@gmail.com>
2022-09-02 16:35:02 +0300

    validation of user email if user with this email already exists

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -6,9 +6,10 @@ class Subscription < ApplicationRecord
   validates :user_email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, unless: -> { user.present? }
 
   validates :user, uniqueness: { scope: :event_id }, if: -> { user.present? }
-  validates :user_email, uniqueness: { scope: :event_id }, unless: -> { user.present? }
-
   validate :not_organizer
+
+  validates :user_email, uniqueness: { scope: :event_id }, unless: -> { user.present? }
+  validate :user_already_exist, unless: -> { user.present? }
 
   def user_name
     user.present? ? user.name : super
@@ -20,5 +21,11 @@ class Subscription < ApplicationRecord
 
   def not_organizer
     errors.add(:user, I18n.t('subscriptions.subscription.errors.owner_cant_be_subscriber')) if user == event.user
+  end
+
+  def user_already_exist
+    if User.find_by(email: user_email).present?
+      errors.add(:user_email, I18n.t('subscriptions.subscription.errors.user_already_exist'))
+    end
   end
 end
