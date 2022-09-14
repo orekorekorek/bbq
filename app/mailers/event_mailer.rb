c40c50a05252b3c7ba28ec6022f2207ebf0c37cf
Kirill Kuznetsov <shapowgorillalord@gmail.com>
2022-09-14 21:10:30 +0300

    default i18n subject added

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -6,20 +6,20 @@ class EventMailer < ApplicationMailer
     @name = subscription.user_name
     @event = subscription.event
 
-    mail to: @event.user.email, subject: t('.subject', event: @event.title)
+    mail to: @event.user.email, subject: default_i18n_subject(event: @event.title)
   end
 
   def comment(comment, email)
     @comment = comment
     @event = comment.event
 
-    mail to: email, subject: t('.subject', event: @event.title)
+    mail to: email, subject: default_i18n_subject(event: @event.title)
   end
 
   def photo(photo, email)
     @photo = photo
     @event = photo.event
 
-    mail to: email, subject: t('.subject', event: @event.title)
+    mail to: email, subject: default_i18n_subject(event: @event.title)
   end
 end
