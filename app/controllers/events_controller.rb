c8c540b6600da9b558e4c20dc8a4f1e9ebc5effd
Kirill Kuznetsov <shapowgorillalord@gmail.com>
2022-09-28 12:57:54 +0300

    refactor

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,7 @@
 class EventsController < ApplicationController
   before_action :authenticate_user!, except: %i[show index]
 
-  before_action :set_event, only: [:show]
+  before_action :set_event, only: %i[show edit update destroy]
 
   before_action :save_pincode_in_cookies, only: [:show]
 
