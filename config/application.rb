04eccebb61d676534c49fa5408fd9d9aab35eb50
Kirill Kuznetsov <shapowgorillalord@gmail.com>
2022-10-02 22:38:49 +0300

    config fix

--- config/application.rb
+++ config/application.rb
@@ -10,9 +10,6 @@ module Bq
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
-    config.hosts << "lvh.me"
-
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
